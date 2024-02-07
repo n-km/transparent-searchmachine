4b248227e4c2884348be7962620d6d8e4b338421
n-km <de.nilskoltermann@gmail.com>
2024-02-07 17:36:17 +0100

    +++

--- test/flow/script.js
+++ test/flow/script.js
@@ -7,6 +7,12 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
     const answer4 = document.getElementById('question4').value.trim().toLowerCase();
     const answer5 = document.getElementById('question5').value.trim().toLowerCase();
     const answer6 = document.getElementById('question6').value.trim().toLowerCase();
+    const answer7 = document.getElementById('question7').value.trim().toLowerCase();
+    const answer8 = document.getElementById('question8').value.trim().toLowerCase();
+    const answer9 = document.getElementById('question9').value.trim().toLowerCase();
+    const answer10 = document.getElementById('question10').value.trim().toLowerCase();
+    const answer11 = document.getElementById('question11').value.trim().toLowerCase();
+    const answer12 = document.getElementById('question12').value.trim().toLowerCase();
 
     let score = 0;
     if (answer1 === 'Holz' || answer1 === 'holz') {
@@ -27,8 +33,26 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
     if (answer6 === 'todkrank') {
         score++;
       }
-  
-    const totalQuestions = 6;
+      if (answer7 === '84') {
+        score++;
+      }
+      if (answer8 === '48') {
+        score++;
+      }
+      if (answer9 === '63') {
+        score++;
+      }
+      if (answer10 === '14') {
+        score++;
+      }
+      if (answer11 === '50') {
+        score++;
+      }
+      if (answer12 === '4') {
+        score++;
+      }
+
+    const totalQuestions = 12;
     const percentage = (score / totalQuestions) * 100;
   
     document.getElementById('result').innerHTML = `You have reached ${percentage}%  <br> <p>Please take now a Screenshot to send it to our E-Mail</p> <br> <form action="thx.html"><button type="submit">Next</button></form>`;
